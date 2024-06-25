9451063471db6780c6db6a263b233fc9a7aa2ca3
Sarthak Mishra <msarthak785@gmail.com>
2024-06-24 22:06:07 -0400

    response rendering minor changes

--- src/pages/main/response.tsx
+++ src/pages/main/response.tsx
@@ -4,11 +4,25 @@ export const Response = () => {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const text = queryParams.get("text");
+    let res;
+
+    if(text) {
+        res = text.split("**");
+    }
 
     return(
         <div className="px-16 font-semibold space-y-2">
             <h1 className="text-2xl">Response: </h1>
-            <p className="text-lg border-y-4 border-green-400">{ text }</p>
+            <p className="text-lg space-y-5">
+                { res?.map((temp) => (
+                    temp.length < 20 ? (
+                        <p className="border-b-4 border-green-400">{temp}</p>
+                    ):(
+                        <p>{temp}</p>
+                    )
+                )) }
+            </p>
+            
             <div>
                 <Link to="/main" className="text-lg font-semibold cursor-pointer bg-green-200 border-[2px] border-gray-400 p-1 rounded-lg hover:bg-green-500 hover:text-white">
                     Generate Again
